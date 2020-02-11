4aa503c7a01a3d51db4b13ec64cab95c6a83dc00
shareleb <2524214961@qq.com>
2020-02-11 10:41:49 +0800

    二叉搜索树main.cpp

---
+++ 二叉排序树的实现/二叉排序树的实现/main.cpp
@@ -0,0 +1,29 @@
+﻿#include "binaryTree.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	leb::BinarySortTree<int> bst;
+
+	bst.insert(5);
+	bst.insert(2);
+	bst.insert(8);
+	bst.insert(0);
+	bst.insert(1);
+	bst.insert(4);
+	bst.insert(7);
+	bst.insert(9);
+	bst.insert(3);
+	bst.insert(6);
+
+	bst.erase(7);
+	vector<int> v = bst.InOrder();
+
+	for (auto & i : v)
+	{
+		cout << i << ' ';
+	}
+	system("pause");
+	return 0;
+}
