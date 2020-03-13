e520cd5c1ce99c3859a9f8704b1315725bb95f29
shareleb <2524214961@qq.com>
2020-03-13 22:27:12 +0800

    12

---
+++ practice/practice/file.c
@@ -0,0 +1,35 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<stdlib.h>
+////c语言文件操作
+//
+//int main()
+//{
+//	FILE * fp=fopen("file.c","r");
+//	if (fp == NULL)
+//	{
+//		printf("打开错误\n");
+//	}
+//	char buf[256];
+//
+//	while (fgets(buf, 255, fp) != NULL)
+//	{
+//		printf("%s", buf);
+//	}
+//	
+//
+//	//文件读写
+//
+//	system("pause");
+//	return 0;
+//
+//
+//	//写文件
+//	int fputc(int n, FILE * fp);  //给fp文件写一个n ，返回的也是n   失败返回EOF
+//	int fputs(char *s ,FILE*fp)
+//
+//
+//
+//
+//		
+//}
