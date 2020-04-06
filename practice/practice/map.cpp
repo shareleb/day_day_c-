b78e31eda7b1daff2bf809d0da971769b25ca49d
shareleb <2524214961@qq.com>
2020-04-06 21:30:26 +0800

    56

---
+++ practice/practice/map.cpp
@@ -0,0 +1,46 @@
+////#include<iostream>
+////#include<unordered_map>
+////#include<map>
+////using namespace std;
+////
+////
+////int main()
+////{
+////	/*unordered_map<int, int>m;
+////		m[1] = 11;
+////		m[0] = 10;
+////		m[1] = 13;
+////		m[2] = 12;*/
+////	map<int, int>m;
+//////	cout << m[0];
+////	m[0] = 10;
+////	//cout << m[0];
+////	m[0] = 100;
+////	//m.insert(pair<int, int>(0, 100));
+////	cout << m[0] << endl;
+////	
+////	/*for (auto i : m) {
+////		cout << i.first << " " << i.second << endl;
+////	}*/
+////
+////
+////	system("pause");
+////	return 0;
+////}
+//
+//#include<iostream>
+//#include<string>
+//#include<cstring>
+//#include<vector>
+//using namespace std;
+//
+//
+//int main(){
+//
+//	string s;
+//	cout << sizeof(s) << endl;
+//
+//	system("pause");
+//	return 0;
+//
+//}
