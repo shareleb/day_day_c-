3fabe44d01e4ee15523061d1d4e7da423d23b1fc
shareleb <2524214961@qq.com>
2020-04-06 21:28:37 +0800

    21

---
+++ practice/practice/heap.h
@@ -0,0 +1,20 @@
+//#pragma once
+//typedef int DataType;
+//
+//typedef struct Heap {
+//	DataType *data;  //数据域
+//	int size;       //数据个数
+//	int capacity;   //容量
+//
+//}Heap;
+//
+//void HeapInit(Heap * hp, DataType *a, int n); //初始化 
+//void HeapDestory(Heap * hp);
+//void HeapPush(Heap * hp, DataType x);
+//void HeapPop(Heap* hp);
+//DataType HeapTop(Heap*hp);
+//int HeapSize(Heap*hp);
+//int HeapEmpty(Heap*hp);
+//
+//void HeapSort(Heap*hp);
+//void HeapPrint(Heap*hp);
