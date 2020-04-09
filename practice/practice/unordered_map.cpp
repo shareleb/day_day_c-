eef7b4b58c4d3a6081c21c0493a9749a128d3e23
shareleb <2524214961@qq.com>
2020-04-09 21:41:33 +0800

    3

---
+++ practice/practice/unordered_map.cpp
@@ -0,0 +1,46 @@
+//#include<iostream>
+//#include<unordered_map>
+//
+//using namespace std;
+////
+////void test01() {
+////	m[1] = 10;
+////	m[20] = 20;
+////	m[3] = 30;
+////	m[5] = 18;
+////	for (auto i : m) {
+////		cout << i.first << "  " << i.second << endl;
+////	}
+////	cout << "*******************" << endl;
+////	m[5] = 24;
+////	cout << m[5] << endl;
+////	m.insert(pair<int, int>(5, 14));
+////	cout << m[5] << endl;
+////
+////	auto  i = m.find(3);
+////	cout << i->first << " " << i->second << endl;
+////	cout << m.count(3) << endl;
+////
+////}
+//
+//void test02() {
+//	unordered_map<int, int>m;
+//	m[1] = 10;
+//	m[2] = 20;
+//	m.insert(3,5);
+//	for (auto i : m) {
+//				cout << i.first << "  " << i.second << endl;
+//		}
+//
+//
+//}
+//
+//
+//
+//int main() {
+//
+//	cout <<" \1" << endl;
+//
+//	system("pause");
+//	return 0;
+//}
