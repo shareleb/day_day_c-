c2eb1d327fed9f1c9519eb8f06abbee8ea15dc80
shareleb <2524214961@qq.com>
2020-04-15 21:10:08 +0800

    1

---
+++ grow/grow/effective.cpp
@@ -0,0 +1,22 @@
+//#include<iostream>
+//using namespace std;
+//
+//
+//int main()
+//{
+//
+//	cout << sizeof(int) << endl;
+//	cout << sizeof(float) << endl;
+//	cout << sizeof(double) << endl;
+//	cout << sizeof(char) << endl;
+//	cout << sizeof(long) << endl;
+//	cout << sizeof(long long) << endl;
+//
+//
+//
+//
+//
+//	system("pause");
+//
+//	return 0;
+//}
