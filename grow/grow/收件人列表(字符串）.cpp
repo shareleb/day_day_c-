35712db16590bf2895be95dcb4cd2e856a0534bd
shareleb <2524214961@qq.com>
2020-04-15 20:56:48 +0800

    15

---
+++ grow/grow/收件人列表(字符串）.cpp
@@ -0,0 +1,40 @@
+//#include<iostream>
+//#include<string>
+//#include<vector>
+//using namespace std;
+//
+//int main() {
+//
+//	vector<string>v;
+//	int n;
+//	cin >> n;
+//	getchar();
+//	for (int i = 0; i < n; i++) {
+//		string  s;
+//
+//		getline(cin, s);
+//		if (s.find(',')!= string::npos){
+//			s.insert(0, 1, '"');
+//			s.push_back('"');
+//			v.push_back(s);
+//				
+//		}
+//		else {
+//			v.push_back(s);
+//		}
+//
+//
+//	}
+//
+//
+//	for (int i = 0; i < v.size()-1; i++) {
+//		cout << v[i] << ", ";
+//	}
+//	cout << v[v.size() - 1];
+//
+//
+//
+//
+//
+//	return 0;
+//}
